8262459189de16311306ae9895221e34d814102d
“MSyahidAlFajri” <masyahida4@gmail.com>
2023-12-02 03:44:19 +0700

    memperbaiki login

--- js/loginfunc.js
+++ js/loginfunc.js
@@ -3,9 +3,9 @@ import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
 import {setCookieWithExpireHour} from "https://jscroot.github.io/cookie/croot.js";
 
 export default function PostSignUp(){
-    let target_url = "https://asia-southeast2-msyahid.cloudfunctions.net/Login";
+    let target_url = "https://asia-southeast2-msyahid.cloudfunctions.net/LoginAdmin";
     let tokenkey = "token";
-    let tokenvalue = "9109a567f9e6aa5fa90f8e6610d43d99de52bc0bc5b38ea884eedcd2f54ccaa7"; 
+    let tokenvalue = "5804dcddb5fd6f93a97c9541e21eb668beafc993e5115f77152e191f08da15e2"; 
     let datainjson = {
         "username": getValue("username"),
         "password": getValue("password")
