5b4ef570209a5f0949325623131b9e5cbaec8b73
“MSyahidAlFajri” <masyahida4@gmail.com>
2023-12-01 23:49:46 +0700

    update frontend

--- admin/js/getfunc.js
+++ admin/js/getfunc.js
@@ -7,22 +7,22 @@ export let tableRowClass="content is-size-6";
 export let tableTemplate=`
 <td>                
 <label class="checkbox">
-  <input type="checkbox">
+  <input  class="checkbox" type="checkbox">
 </label>
 </td>
 <td data-label="Name">#NAMA#</td>
 <td data-label="Harga">#HARGA#</td>
 <td data-label="Deskripsi">#DESKRIPSI#</td>
 <td class="is-actions-cell">
-                  <div class="buttons is-right">
-                    <button class="button is-small is-primary" type="button">
-                      <span class="icon"><i class="mdi mdi-eye"></i></span>
-                    </button>
-                    <button class="button is-small is-danger jb-modal" data-target="sample-modal" type="button">
-                      <span class="icon"><i class="mdi mdi-trash-can"></i></span>
-                    </button>
-                  </div>
-                </td>
+<div class="buttons is-right">
+<button class="button is-small is-primary" type="button">
+<span class="icon"><i class="mdi mdi-eye"></i></span>
+</button>
+<button class="button is-small is-danger jb-modal" data-target="sample-modal" type="button">
+<span class="icon"><i class="mdi mdi-trash-can"></i></span>
+</button>
+</div>
+</td>
 ` 
 
 export function responseData(results){
@@ -35,3 +35,21 @@ export function isiRow(value){
     console.log(content);
     addChild("produk",tableTag,tableRowClass,content);
 }
+
+/* Modal: open */
+Array.from(document.getElementsByClassName('jb-modal')).forEach(el => {
+    el.addEventListener('click', e => {
+      const modalTarget = e.currentTarget.getAttribute('data-target')
+  
+      document.getElementById(modalTarget).classList.add('is-active')
+      document.documentElement.classList.add('is-clipped')
+    })
+  });
+  
+  /* Modal: close */
+  Array.from(document.getElementsByClassName('jb-modal-close')).forEach(el => {
+    el.addEventListener('click', e => {
+      e.currentTarget.closest('.modal').classList.remove('is-active')
+      document.documentElement.classList.remove('is-clipped')
+    })
+  })
