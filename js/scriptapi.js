fb96f8cc06018e4133c169fcfdbbc31d0952f12a
GilarYa <gilarwahibul@gmail.com>
2023-12-02 23:49:03 +0700

    push loading

--- js/scriptapi.js
+++ js/scriptapi.js
@@ -8,3 +8,8 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .catch(error => console.error('Error fetching daily quote:', error));
 });
+    // Menghilangkan animasi loading dan menampilkan konten utama setelah 3 detik
+    setTimeout(function () {
+        document.getElementById('loader-wrapper').style.display = 'none';
+        document.getElementById('main-content').style.display = 'block';
+      }, 3000); 
