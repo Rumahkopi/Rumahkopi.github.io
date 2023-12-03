1b573ec124508d66db93f9ce575fe3df615273b3
GilarYa <gilarwahibul@gmail.com>
2023-12-04 02:58:40 +0700

    push bentarr

--- js/scriptb.js
+++ js/scriptb.js
@@ -1,11 +1,8 @@
+  // Retrieve selected product data from localStorage
+    var selectedProduct = JSON.parse(localStorage.getItem('selectedProduct'));
 
-// Fetch product details from URL parameters
-var urlParams = new URLSearchParams(window.location.search);
-var product = urlParams.get('product');
-var quantity = urlParams.get('quantity');
-var total = urlParams.get('total');
-
-// Fill in the form with product details
-document.getElementById('product').value = product;
-document.getElementById('quantity').value = quantity;
-document.getElementById('total').textContent = total; // Set the text content of the <span> element
+    // Display product information on the checkout page
+    document.getElementById('productName').value = selectedProduct.name;
+    document.getElementById('productPrice').value = selectedProduct.price;
+    document.getElementById('productQuantity').value = selectedProduct.quantity;
+    document.getElementById('productTotal').value = selectedProduct.total;
