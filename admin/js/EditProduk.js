e842353115dc658a528bf725e2cc0a516724bb5a
“MSyahidAlFajri” <masyahida4@gmail.com>
2023-12-04 22:51:21 +0700

    update

--- admin/js/EditProduk.js
+++ admin/js/EditProduk.js
@@ -6,24 +6,24 @@ export const isiData = (results) => {
       {id: "stok", path: "stok"},
     ];
   
-    // console.log("isiData - Input:", results);
+    console.log("isiData - Input:", results);
   
     dataProduk.forEach(({ id, path, index, property }) => {
       const inputElement = document.getElementById(id);
-      // console.log(`Updating element with ID ${id} with data from path ${path}`);
+      console.log(`Updating element with ID ${id} with data from path ${path}`);
       const value = getNestedValue(results, path, index, property);
-      // console.log(`Setting value for ${id}:`, value);
+      console.log(`Setting value for ${id}:`, value);
       inputElement.value = value;
     });
   }
   
   const getNestedValue = (obj, path, index, property) => {
-    // console.log("getNestedValue - Input:", obj, path, index, property);
+    console.log("getNestedValue - Input:", obj, path, index, property);
     const value = path
       .split(".")
       .reduce((value, key) => (value && value[key] !== undefined ? value[key] : ""), obj);
   
-    // console.log("getNestedValue - Intermediate Value:", value);
+    console.log("getNestedValue - Intermediate Value:", value);
   
     if (
       Array.isArray(value) &&
