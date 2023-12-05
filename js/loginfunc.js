7ad4596555ced8bc328cfabe847c79ca35433df2
“MSyahidAlFajri” <masyahida4@gmail.com>
2023-12-05 23:29:51 +0700

    Rombak Frontend

--- js/loginfunc.js
+++ js/loginfunc.js
@@ -2,7 +2,7 @@ import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
 import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
 
-export default function PostSignUp() {
+export default function Login() {
   // Display loading overlay
   showLoadingOverlay();
 
